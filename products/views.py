f12d2fb6cfc0434955b73b3e2d8e037bdea72037
AnwarFA <anwaralaiban123@gmail.com>
2022-10-17 01:56:54 +0300

    Finished the Templates task

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+
+import products
 from .models import Product
 
 # Create your views here.
@@ -9,6 +12,27 @@ def get_home(request):
 
 def get_product(request, product_id):
     products = Product.objects.get(id=product_id)
-    return HttpResponse(
-        f"name:{products.name}, price:{products.price}, description:{products.description}"
-    )
+    context = {
+        "product": {
+            "name": products.name,
+            "id": products.id,
+            "description": products.description,
+            "price": products.price,
+        }
+    }
+    return render(request, "product-detail.html", context)
+
+
+def get_products(request):
+    products = Product.objects.all()
+    thenew_product = []
+    for product in products:
+        thenew_product.append(
+            {
+                "name": products.name,
+                "description": products.description,
+                "price": products.price,
+            }
+        )
+    context = {"products": thenew_product}
+    return render(request, "product-list.html", context)
--- productshop/urls.py
+++ productshop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products import get_home, get_product
+from products.views import get_products
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("home/", get_home),
-    path("products/", get_product),
-    path("products/<int:product_id>/", get_product),
+    path("home/", get_home, name="home"),
+    path("products/", get_products, name="product_list"),
+    path("products/<int:product_id>/", get_product, name="product_detail"),
 ]
